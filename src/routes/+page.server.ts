ec72edfb9dedeedd71ac8940afb9c810c23b56cd
jovany velez <jovany.velez@udea.edu.co>
2023-06-13 09:10:42 -0500

    a3

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,15 +5,6 @@ export const actions = {
 		cookies.set('session', "Soy la cookie", {
       // send cookie for every page
       path: '/',
-      // server side only cookie so you can't use `document.cookie`
-      httpOnly: true,
-      // only requests from same site can send cookies
-      // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
-      sameSite: 'none',
-      // only sent over HTTPS in production
-      secure: false,
-      // set cookie to expire after a month
-      maxAge: 60 * 60 * 24 * 30,
     })
     throw redirect(302,"/recibir")
 	},
